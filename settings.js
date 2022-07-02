40a25a2ab82ed79e3934c8296449f84f94a82b09
timegoul <108570977+timegoul@users.noreply.github.com>
2022-07-02 02:04:40 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,9 +22,9 @@ global.APIKeys = {
 global.alivemsg = `My name is RED DRAGON🔥
 My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
-global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
+global.owner = ['+940718906236'] //Owner number in aive msg
+global.premium = ['+940718906236'] //Owner Number info
+global.ownernomer = '+940718906236' //Owner Number <<<
 
 global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
 global.botname = ' ☬RED༒DRAGON' //Bot Name
